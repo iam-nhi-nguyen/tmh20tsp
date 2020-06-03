c67f157155732deae9271f3b89bbfd9fe14c67ee
nguyennganhi <49206582+nguyennganhi@users.noreply.github.com>
2020-06-04 02:20:25 +0700

    Update ga.js

--- ga.js
+++ ga.js
@@ -45,7 +45,7 @@ function calculateFitness() {
       var orderA = pickOne(population, fitness);
       var orderB = pickOne(population, fitness);
       var order = crossOver(orderA, orderB);
-      mutate(order, 0.1);
+      mutate(order, 0.2);
       newPopulation[i] = order;
     }
     population = newPopulation;
