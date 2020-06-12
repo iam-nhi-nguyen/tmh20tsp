cb11ce2c10007bec68711c30f70bc1f57ee5c16d
nguyennganhi <49206582+nguyennganhi@users.noreply.github.com>
2020-06-12 17:23:49 +0700

    Update vertices.js

--- functions/vertices.js
+++ functions/vertices.js
@@ -2,18 +2,61 @@
 // Traveling Salesman Problem
 
 function findVertices(){
-    if(random){
-		randomVertices();
-	}
+    totalVertices = parseInt(document.getElementById('numVer').value);
+
+    if(rand){
+        randomVertices();
+        randomDistances();
+    }
+    else{
+        fixedVertices();
+        //randomDistances();
+        if(document.getElementById('distMat').value == ""){
+            distances = distancesSample;
+        }
+        else{
+            fixedDistances();
+        }
+    }
 }
 
 function randomVertices(){
     vertices = [];
-    totalVertices = document.getElementById('numVer').value;
 
     // Get random vertices
     for (var i = 0; i < totalVertices; i++) {
         var v = createVector(random(panelWidth - 2*padding), random(panelHeight - 2*padding));
         vertices[i] = v;
     }
+}
+
+function randomDistances(){
+    distances = [];
+
+    for(var i = 0; i < totalVertices; i++){
+        distances_i = [];
+        for(var j = 0; j < totalVertices; j++){
+            var d = dist(vertices[i].x, vertices[i].y, vertices[j].x, vertices[j].y);
+            distances_i.push(d);
+        }
+        distances.push(distances_i);
+    }
+}
+
+function fixedVertices(){
+    vertices = [];
+
+    for (var i = 0; i < totalVertices; i++) {
+        var v = createVector(verticesSample[i][0]*35, verticesSample[i][1]*35);
+        vertices[i] = v;
+    }
+}
+
+function fixedDistances(){
+    tmp = document.getElementById('distMat').value;
+    tmp = tmp.split("\n");
+    for(var i = 0; i < tmp.length; i++){
+        tmp[i] = tmp[i].split(" ").map(numStr => parseInt(numStr));
+    }
+    distances = tmp;
 }
